c26e4aabc8d820fc0d301419acac0d582714c184
Michael Giddings <mikegiddings@gmail.com>
2020-08-28 12:53:58 -0400

    refactored loadTasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -239,44 +239,15 @@ Converts tasks from the stringified format back into an array of objects
 
 Iterates through tasks array and creates task elements on the page from it */
 
-    tasks = localStorage.getItem("tasks");
-    if(!tasks) {
+    var savedTasks = localStorage.getItem("tasks");
+    if(!savedTasks) {
         tasks = [];
         return false;
     }
-    tasks = JSON.parse(tasks);
-    for(var i=0;i<tasks.length;i++) {
-        taskIdCounter = tasks[i].id;
-
-        var listItemEl = document.createElement("li");
-        listItemEl.setAttribute("class","task-item");
-        listItemEl.setAttribute("data-task-id", tasks[i].id);
-        listItemEl.setAttribute("draggable","true");
-
-        var taskInfoEl = document.createElement("div");
-        taskInfoEl.setAttribute("class","task-info");
-        taskInfoEl.innerHTML= "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
-        listItemEl.appendChild(taskInfoEl);
-
-        var taskActionsEl = createTaskActions(tasks[i].id);
-        listItemEl.appendChild(taskActionsEl);
-
-        if(tasks[i].status === "to do"){
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
-            tasksToDoEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status === "in progress") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
-            tasksInProgressEl.appendChild(listItemEl);
-        }
-        else if(tasks[i].status === "completed") {
-            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
-            tasksCompletedEl.appendChild(listItemEl);
-        }
-        taskIdCounter++;
-        console.log(listItemEl);
+    savedTasks = JSON.parse(savedTasks);
+    for(var i=0;i<savedTasks.length;i++) {
+        createTaskEl(savedTasks[i]);
     }
-
 };
 
 formEl.addEventListener("submit",taskFormHandler);
