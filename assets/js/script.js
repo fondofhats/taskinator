fbe98907c7a7c5d37944e6c7a8c786aa19724938
Michael Giddings <mikegiddings@gmail.com>
2020-08-28 12:48:11 -0400

    added get from localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -231,6 +231,54 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+var loadTasks = function() {
+
+/* Gets task items from localStorage
+
+Converts tasks from the stringified format back into an array of objects
+
+Iterates through tasks array and creates task elements on the page from it */
+
+    tasks = localStorage.getItem("tasks");
+    if(!tasks) {
+        tasks = [];
+        return false;
+    }
+    tasks = JSON.parse(tasks);
+    for(var i=0;i<tasks.length;i++) {
+        taskIdCounter = tasks[i].id;
+
+        var listItemEl = document.createElement("li");
+        listItemEl.setAttribute("class","task-item");
+        listItemEl.setAttribute("data-task-id", tasks[i].id);
+        listItemEl.setAttribute("draggable","true");
+
+        var taskInfoEl = document.createElement("div");
+        taskInfoEl.setAttribute("class","task-info");
+        taskInfoEl.innerHTML= "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+        listItemEl.appendChild(taskInfoEl);
+
+        var taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+
+        if(tasks[i].status === "to do"){
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if(tasks[i].status === "in progress") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if(tasks[i].status === "completed") {
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter++;
+        console.log(listItemEl);
+    }
+
+};
+
 formEl.addEventListener("submit",taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
@@ -239,3 +287,5 @@ pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
 pageContentEl.addEventListener("dragleave", dragLeaveHandler);
 
+
+loadTasks();
